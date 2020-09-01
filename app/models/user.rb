d254cf8669ec2c8980e0a0a0fd8738b176f0204b
Richard Wise <orsusbass@gmail.com>
2020-08-31 21:29:33 -0500

    user factory and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_person_name
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
 end
