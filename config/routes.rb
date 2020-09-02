9d6a35bdd061bbaf31d57d9018b888c8bc468b69
Richard Wise <orsusbass@gmail.com>
2020-09-02 10:53:58 -0500

    create books controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :users do
+    resources :books
+  end
+
+  scope "books" do
+    get "search", to: "books#search"
+  end
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
