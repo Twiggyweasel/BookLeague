5fba05a36a223e7ff251eec6529002d06f31c373
Richard Wise <orsusbass@gmail.com>
2020-09-10 19:28:31 -0500

    controller and views for memberships

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,15 @@ Rails.application.routes.draw do
     get "search", to: "books#search"
   end
 
-  resources :clubs
+  resources :clubs do
+    resources :memberships
+  end
   scope 'users/:id' do
     resources :clubs, only: :index
+    resources :memberships, only: :index
   end
+
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
